6adc58aa82d2b4544eec6fa59fe2fe2d735f1754
Oumadenis1779 <dennies.ouma@student.moringaschool.com>
2023-10-02 10:40:45 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch ('https://anapioficeandfire.com/api/books')
+  .then((response) => {
+    if(!response.ok){
+      throw new Error ('Network response was not ok');
+    }
+    return response.json();
+  })
+  .then((data) => {
+    
+    renderBooks(data);
+  })
 }
 
-function renderBooks(books) {
+
+document.addEventListener('DOMContentLoaded', function() {
+  fetchBooks();
+});
+
+function renderBooks(books){
   const main = document.querySelector('main');
-  books.forEach(book => {
+  books.forEach(book =>{
     const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
+    h2.innerHeight = book.name;
     main.appendChild(h2);
   });
 }
